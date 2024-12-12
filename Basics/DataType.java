6e5d1cf000bcefa2b68d3a49b47aba9fe2ba7cdc
Rishav737 <rajput7122003@gmail.com>
2024-12-12 22:02:57 +0530

    First class

--- Basics/DataType.java
+++ Basics/DataType.java
@@ -1,6 +1,77 @@
+char datatype
+
 public class DataType {
     public static void main(String[] args) {
         char b = 'o';
         System.out.println("char :" + b);
+    }}
+
+int DataType
+
+    public class First {
+        public static void main(String[] args) {
+            int i = 7;
+            System.out.println("int :" + i);
+        }
+
     }
+
+float datatype
+
+    public class First {
+        public static void main(String[] args) {
+            float f = 655.5f;
+            System.out.println("float :" + f);
+        }
+
+    }
+
+boolean datatype
+
+    public class First {
+        public static void main(String[] args) {
+            boolean b = true;
+            System.out.println("boolean :" + b);
+        }
+
+    }
+
+byte datatype
+
+    public class First {
+        public static void main(String[] args) {
+            byte c = 126;
+            System.out.println("byte :" + c);
+        }
+
+    }
+
+short datatype
+
+    public class First {
+        public static void main(String[] args) {
+            short d = 145;
+            System.out.println("short :" + d);
+        }
+
+    }
+
+long datatype
+
+    public class First {
+        public static void main(String[] args) {
+            long l = -145;
+            System.out.println("long :" + l);
+        }
+
+    }
+
+double datatype
+
+    public class First {
+        public static void main(String[] args) {
+            double d = 12.3;
+            System.out.println("double :" + d);
+        }
+
 }
