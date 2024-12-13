30c68335978488d93fc755bf9c3cffc15b9a6d71
Rishav737 <rajput7122003@gmail.com>
2024-12-13 14:25:57 +0530

    program for check eligible for voter id or not

---
+++ Control_statement/Voterid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Voterid {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the age");
+        int age = sc.nextInt();
+        if (age >= 18) {
+            System.out.println("eligible for vote");
+
+        } else {
+            System.out.println("not eligible for vote");
+        }
+    }
+}
