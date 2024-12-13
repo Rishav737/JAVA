7515920ddc116166e2d8b7303c9e2b76e6da8ad2
Rishav737 <rajput7122003@gmail.com>
2024-12-13 14:44:22 +0530

    to check maximum number

---
+++ Control_statement/Maximumnumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Maximumnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of a");
+        int a = sc.nextInt();
+        if (a > 10) {
+            System.out.println("a is greater than 10");
+
+        } else {
+            System.out.println("input is smaller than 10");
+        }
+    }
+}
