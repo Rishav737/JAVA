2bd8fa38a64eeb0fc3637c1b584181382b732ef2
Rishav737 <rajput7122003@gmail.com>
2024-12-18 12:06:26 +0530

    Method1

---
+++ Oops/Method1.java
@@ -0,0 +1,12 @@
+class A {
+    void show() {
+        System.out.println("hello world");
+    }
+}
+
+public class Method1 {
+    public static void main(String[] args) {
+        A p = new A();
+        p.show();
+    }
+}
