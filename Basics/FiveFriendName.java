507c0029be9a2cab2a74cc63c37bc8e516120d5b
Rishav737 <rajput7122003@gmail.com>
2024-12-20 15:42:55 +0530

    FiveFriendName

---
+++ Basics/FiveFriendName.java
@@ -0,0 +1,10 @@
+public class FiveFriendName {
+    public static void main(String[] args) {
+        System.out.println("Shivam");
+        System.out.println("Jaipal");
+        System.out.println("aman");
+        System.out.println("adarsh");
+        System.out.println("sonu");
+    }
+
+}
