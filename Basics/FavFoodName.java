d3a1627e22eae21a204a62cd63836dbea9d0fa35
Rishav737 <rajput7122003@gmail.com>
2024-12-20 15:48:02 +0530

    FavFoodName

---
+++ Basics/FavFoodName.java
@@ -0,0 +1,10 @@
+public class FavFoodName {
+    public static void main(String[] args) {
+        System.out.println("sandwich");
+        System.out.println("noodels");
+        System.out.println("maggi");
+        System.out.println("pizza");
+        System.out.println("pasta");
+    }
+
+}
