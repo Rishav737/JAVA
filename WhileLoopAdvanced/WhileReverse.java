3ce5bf699d3a2cb57dddb9e7e6a9d1e3d03a6922
Rishav737 <rajput7122003@gmail.com>
2024-12-23 16:27:34 +0530

    Whilereverse.java

---
+++ WhileLoopAdvanced/WhileReverse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class WhileReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int sum = 0, rem;
+        int temp = n;
+        while (n > 0) {
+            rem = n % 10;
+            sum = sum * 10 + rem;
+            n = n / 10;
+        }
+        System.out.println("Reverse of " + temp + " is " + sum);
+    }
+}
